e6cf9d691b4aa82be4c1653858ee143cc2616f6e
Muhammad Abdullah Waheed <abdullah.waheed@arbisoft.com>
2024-05-03 19:00:14 +0500

    feat: added html plugin

--- DatadogPlugin.js
+++ DatadogPlugin.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 class DatadogPlugin {
   constructor(options) {
@@ -12,31 +13,29 @@ class DatadogPlugin {
     };
   }
 
-  apply(compiler){
+  apply(compiler) {
     compiler.hooks.compilation.tap('DatadogPlugin', (compilation) => {
-      compilation.hooks.processAssets.tap(
-        {
-          name: 'DatadogPlugin',
-          stage: 'additional'
-        }, () => 
-        {
-        let customCode = '';
-
-        try {
-          // Read the contents of the code file
-          console.error('\n\n\n\n\n\n\nstart');
-          customCode = fs.readFileSync('../node_modules/datadog-testing-plugin/DatadogInit.js', 'utf8');
-          
-        } catch (error) {
-          console.error(`Error reading file: ${error}`);
-          return;
-        }
-        console.error('\n\n\n\n\n\n\nsuccess');
-        compilation.emitAsset('custom.js', {
-          source: () => customCode,
-          size: () => customCode.length
-        });
-      });
+      HtmlWebpackPlugin.getHooks(compilation).alterAssetTagGroups.tap(
+        'DatadogPlugin',
+        (data) => {
+          try {
+            // Read the contents of the code file
+            console.error('\n\n\n\n\n\n\nstart');
+            customCode = fs.readFileSync('../node_modules/datadog-testing-plugin/DatadogInit.js', 'utf8');
+            
+          } catch (error) {
+            console.error(`Error reading file: ${error}`);
+            return;
+          }
+          console.error('\n\n\n\n\n\n\nsuccess');
+          const newRelicScriptTag = HtmlWebpackPlugin.createHtmlTagObject(
+            'script',
+            { type: 'text/javascript' },
+            customCode,
+          );
+          data.headTags.unshift(newRelicScriptTag);
+        },
+      );
     });
   }
 }
