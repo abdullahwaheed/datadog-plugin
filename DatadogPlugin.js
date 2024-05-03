395ac75c2914106816e5c03f95a2ffd90527678e
Muhammad Abdullah Waheed <abdullah.waheed@arbisoft.com>
2024-05-03 20:56:13 +0500

    refactor: tested embedding code with HtmlWebpackPlugin

--- DatadogPlugin.js
+++ DatadogPlugin.js
@@ -1,5 +1,26 @@
 const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { minify } = require('terser');
+
+async function minifyCode(code) {
+  try {
+    const result = await minify(code, {
+      ecma: 1, // Use ECMAScript 6 syntax
+      keep_classnames: true, // Preserve class names
+      keep_fnames: true, // Preserve function names
+      mangle: false, // Disable variable name mangling
+      compress: true, // Enable code compression
+      output: {
+          comments: false // Remove comments
+      }
+  });
+    return result.code;
+  } catch (error) {
+    console.error('Error during code minification:', error);
+    return null;
+  }
+}
+
 
 class DatadogPlugin {
   constructor(options) {
@@ -18,6 +39,7 @@ class DatadogPlugin {
       HtmlWebpackPlugin.getHooks(compilation).alterAssetTagGroups.tap(
         'DatadogPlugin',
         (data) => {
+          let customCode = '';
           try {
             // Read the contents of the code file
             console.error('\n\n\n\n\n\n\nstart');
@@ -28,12 +50,14 @@ class DatadogPlugin {
             return;
           }
           console.error('\n\n\n\n\n\n\nsuccess');
-          const newRelicScriptTag = HtmlWebpackPlugin.createHtmlTagObject(
-            'script',
-            { type: 'text/javascript' },
-            customCode,
-          );
-          data.headTags.unshift(newRelicScriptTag);
+          minifyCode(customCode).then(minifiedCode => {
+            const newRelicScriptTag = HtmlWebpackPlugin.createHtmlTagObject(
+              'script',
+              { type: 'text/javascript' },
+              minifiedCode,
+            );
+            data.headTags.unshift(newRelicScriptTag);
+          });
         },
       );
     });
