00514e3c144be02faea3ac0d605797fc36a9dee1
Muhammad Abdullah Waheed <abdullah.waheed@arbisoft.com>
2024-05-03 18:55:31 +0500

    refactor: last working

--- DatadogPlugin.js
+++ DatadogPlugin.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 class DatadogPlugin {
   constructor(options) {
     this.options = {
@@ -11,27 +13,30 @@ class DatadogPlugin {
   }
 
   apply(compiler){
-    compiler.hooks.emit.tap('DatadogPlugin', (compilation) => {
-      let customCode = '';
+    compiler.hooks.compilation.tap('DatadogPlugin', (compilation) => {
+      compilation.hooks.processAssets.tap(
+        {
+          name: 'DatadogPlugin',
+          stage: 'additional'
+        }, () => 
+        {
+        let customCode = '';
 
-      try {
-        // Read the contents of the code file
-        customCode = fs.readFileSync('DatadogInit.js', 'utf8');
-      } catch (error) {
-        console.error(`Error reading file: ${error}`);
-        return;
-      }
-      compilation.assets['custom.js'] = {
-        source: () => customCode,
-        size: () => customCode.length
-      };
-      // const customCode = this.options.customCode;
-
-      // Add the custom code to the generated bundle
-      // compilation.assets['custom.js'] = {
-      //   source: () => customCode,
-      //   size: () => customCode.length
-      // };
+        try {
+          // Read the contents of the code file
+          console.error('\n\n\n\n\n\n\nstart');
+          customCode = fs.readFileSync('../node_modules/datadog-testing-plugin/DatadogInit.js', 'utf8');
+          
+        } catch (error) {
+          console.error(`Error reading file: ${error}`);
+          return;
+        }
+        console.error('\n\n\n\n\n\n\nsuccess');
+        compilation.emitAsset('custom.js', {
+          source: () => customCode,
+          size: () => customCode.length
+        });
+      });
     });
   }
 }
